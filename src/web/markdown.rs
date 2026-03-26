09c674e56f9e8e9fb9bea46524f853ab523a31a7
Alexey <alex.esprit@gmail.com>
2026-03-26 04:01:47 +0300

    Enable table parsing in markdown renderer

--- src/web/markdown.rs
+++ src/web/markdown.rs
@@ -1,7 +1,7 @@
 use pulldown_cmark::{Options, Parser, html};
 
 pub fn render(source: &str) -> String {
-    let parser = Parser::new_ext(source, Options::empty());
+    let parser = Parser::new_ext(source, Options::ENABLE_TABLES);
     let mut html_output = String::new();
     html::push_html(&mut html_output, parser);
     ammonia::clean(&html_output)
