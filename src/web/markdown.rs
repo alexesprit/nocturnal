d77d1f472a7a227ac2ea2f14eda192ee2b759e04
Alexey <alex.esprit@gmail.com>
2026-04-05 10:01:34 +0300

    Render markdown task list checkboxes in web dashboard

--- src/web/markdown.rs
+++ src/web/markdown.rs
@@ -1,8 +1,12 @@
 use pulldown_cmark::{Options, Parser, html};
 
 pub fn render(source: &str) -> String {
-    let parser = Parser::new_ext(source, Options::ENABLE_TABLES);
+    let parser = Parser::new_ext(source, Options::ENABLE_TABLES | Options::ENABLE_TASKLISTS);
     let mut html_output = String::new();
     html::push_html(&mut html_output, parser);
-    ammonia::clean(&html_output)
+    ammonia::Builder::new()
+        .add_tags(&["input"])
+        .add_tag_attributes("input", &["type", "checked", "disabled"])
+        .clean(&html_output)
+        .to_string()
 }
